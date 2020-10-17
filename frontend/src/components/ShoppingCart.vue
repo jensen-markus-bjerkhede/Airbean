7d9bb943e4743ba7d9e10db6078e502b33c07b5e
jensen-markus-bjerkhede <markus.bjerkhede@gmail.com>
2020-10-17 10:52:13 +0200

    WIP

--- frontend/src/components/ShoppingCart.vue
+++ frontend/src/components/ShoppingCart.vue
@@ -1,98 +1,128 @@
 
 <template>
-    <section class="ShoppingCart-box">
-        <section id="ShoppingCart">
-            <h2>Din beställning</h2>
-            <ShoppingCartItem :coffee="coffee" :key="coffee.id" v-for="coffee in shoppingCart"/>
-            <h2 class="total">Total <span class="total-dots"></span>{{totalCost}} kr</h2>
-            <p>inkl moms + drönarleverans</p>
-            <button @click="placeOrder" class="order-button">Take my money!</button>
-        </section>
+  <section class="ShoppingCart-box">
+    <section id="ShoppingCart">
+      <h2>Din beställning</h2>
+      <ShoppingCartItem
+        :coffee="coffee"
+        :key="coffee.id"
+        v-for="coffee in shoppingCart"
+      />
+      <h2 class="total">
+        Total <span class="total-dots"></span>{{ totalCost }} kr
+      </h2>
+      <p>inkl moms + drönarleverans</p>
+      <button @click="placeOrder" class="order-button">Take my money!</button>
     </section>
+  </section>
 </template>
 
 <script>
+import ShoppingCartItem from "@/components/ShoppingCartItem";
 
-    import ShoppingCartItem from '@/components/ShoppingCartItem'
-
-    export default {
-        name: 'ShoppingCart',
-        components: {
-            ShoppingCartItem
-        },
-        computed: {
-            shoppingCart() {
-                return this.$store.getters.shoppingCart
-            },
-            totalCost() {
-                return this.$store.getters.shoppingCart.reduce((acc, item) => acc + item.price * item.quantity, 0)
-            }
-        },
-        methods: {
-            placeOrder() {
-                this.$store.dispatch('placeOrder')
-                this.$router.push('/status')
-            }
-        }
-    }
+export default {
+  name: "ShoppingCart",
+  components: {
+    ShoppingCartItem,
+  },
+  computed: {
+    shoppingCart() {
+      return this.$store.getters.shoppingCart;
+    },
+    totalCost() {
+      return this.$store.getters.shoppingCart.reduce(
+        (acc, item) => acc + item.price * item.quantity,
+        0
+      );
+    },
+  },
+  methods: {
+    placeOrder() {
+      owner = sessionStorage.getItem("user");
+      orderProducts = getOrderProductsFromShoppingCart(); 
+      /*
+		{
+			"id": 1,
+			"quantity": 2
+		},
+		{
+			"id": 3,
+			"quantity": 10
+		},
+		{
+			"id": 2,
+			"quantity": 1
+		}
+      */
+      order = {
+        owner: owner,
+        products: [
+          orderProducts
+        ],
+      };
+      this.$store.dispatch("placeOrder", order);
+      this.$router.push("/status");
+    },
+  },
+};
 </script>
 
 <style lang="scss">
+@import "./../scss/variables";
 
-    @import './../scss/variables';
-
-    .ShoppingCart-box {
-        position: absolute;
-        width: 100%;
-        height: 100%;
-        top: 0;
-        left: 0;
-        right: 0;
-        bottom: 0;
-        background-color: rgba(0,0,0,0.5);
-        z-index: 2;
-        padding: 6rem 1rem;
+.ShoppingCart-box {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: rgba(0, 0, 0, 0.5);
+  z-index: 2;
+  padding: 6rem 1rem;
+}
+#ShoppingCart {
+  background: #ffffff;
+  padding: 3rem 1rem;
+  p {
+    text-align: left;
+  }
+}
+.cart-item {
+  text-align: left;
+  display: flex;
+  .item-quantity {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+    align-items: center;
+    margin-left: 0.5rem;
+    .inc-quantity,
+    .dec-quantity {
+      cursor: pointer;
+      height: 5px;
     }
-    #ShoppingCart {
-        background: #FFFFFF;
-        padding: 3rem 1rem;
-        p {
-            text-align: left;
-        }
+    .dec-quantity {
+      transform: rotate(180deg);
     }
-    .cart-item {
-        text-align: left;
-        display: flex;
-        .item-quantity {
-            display: flex;
-            flex-direction: column;
-            justify-content: space-evenly;
-            align-items: center;
-            margin-left: .5rem;
-            .inc-quantity, .dec-quantity {
-                cursor: pointer;
-                height: 5px;
-            }
-            .dec-quantity {
-                transform: rotate(180deg);
-            }
-        }
-        .item-info {
-            display: flex;
-            flex-direction: column;
-        }
-    }
-    .total {
-        display: flex;
-        text-align: left;
-    }
-    .order-button {
-        width: 248px;
-        height: 55px;
-        background: $airBeanBrown;
-        border-radius: 50px;
-        color: #FFFFFF;
-        font-size: 24px;
-        margin-top: 2.5rem;
-    }
+  }
+  .item-info {
+    display: flex;
+    flex-direction: column;
+  }
+}
+.total {
+  display: flex;
+  text-align: left;
+}
+.order-button {
+  width: 248px;
+  height: 55px;
+  background: $airBeanBrown;
+  border-radius: 50px;
+  color: #ffffff;
+  font-size: 24px;
+  margin-top: 2.5rem;
+}
 </style>
